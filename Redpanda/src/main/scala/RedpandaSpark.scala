171e8559458eff773780e7638a4eed06dfd335e9
Techtacles <emmanueloffisong2002@yahoo.com>
2022-07-21 03:07:13 +0100

    first commit

--- Redpanda/src/main/scala/RedpandaSpark.scala
+++ Redpanda/src/main/scala/RedpandaSpark.scala
@@ -1,5 +1,17 @@
+
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.functions.from_json
+
+import scala.List
+
 object RedpandaSpark {
   import org.apache.spark.sql.SparkSession
+  import org.apache.spark.sql.types._
+  import org.apache.spark.sql.functions._
+
+
+
+
 
   def main(args:Array[String]):Unit= {
     val spark = SparkSession
@@ -7,18 +19,33 @@ object RedpandaSpark {
       .appName("Redpanda Stream")
       .config("spark.master", "local")
       .getOrCreate()
+    import spark.implicits._
+
 
     val redpanda_read = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
       .option("subscribe", "sensor_stream")
-      .option("startingOffsets", "earliest") // From starting
       .load()
-    val redpanda_write = redpanda_read.selectExpr("CAST(key AS String)","CAST(value AS String)",
-    "CAST(topic AS String)","CAST(partition AS String)","CAST(offset as String)")
+
+
+    val sensor_data=redpanda_read.selectExpr( "CAST(value AS STRING)")
+    val stream_1=sensor_data.withColumn("value", split(col("value"), ",")).select(col("value")(0).as("id"),col("value")(3).as("temperature")
+    )
+    val stream_2=sensor_data.withColumn("value",split(col("value"),",")).select(col("value")(0).as("id"),col("value")(3).as("temperature"))
+    val joined_stream=stream_1.union(stream_2)
+    /*
+        val neww=agg.withColumn("id",split(col("id"),",")).select(col("id").getItem(0))
+    */
+    /*val redpanda_write = sensor_data
       .writeStream.format("csv")
-      .option("path", "C:/Users/USER/Desktop/Redpanda article/output").outputMode("append")
-      .option("checkpointLocation", "C:/Users/USER/Desktop/Redpanda article/checkpoint").start().awaitTermination()
+      .option("path", "C:/Users/USER/Desktop/Redpanda article/Streaming-from-redpanda/output").outputMode("append")
+      .option("checkpointLocation", "C:/Users/USER/Desktop/Redpanda article/Streaming-from-redpanda/checkpoint")
+      .start().awaitTermination()*/
+    val to_write=joined_stream.writeStream.format("csv")
+      .option("path", "C:/Users/USER/Desktop/Redpanda article/Streaming-from-redpanda/output").outputMode("append")
+      .option("checkpointLocation", "C:/Users/USER/Desktop/Redpanda article/Streaming-from-redpanda/checkpoint").start()
+      .awaitTermination()
 
   }
 }
